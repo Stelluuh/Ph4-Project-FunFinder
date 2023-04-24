f460e287a70e5009f8f0609b5424670915631b92
stella <sftamesis13@gmail.com>
2023-04-24 16:32:34 -0700

    add navlink to login and logout page with buttons

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
-      
+        <NavLink to="/login">
+            <button>Login</button>
+        </NavLink>
+        <h3> or </h3>
+        <NavLink to="/login">
+            <button>Signup</button>
+        </NavLink>
     </div>
   )
 }
