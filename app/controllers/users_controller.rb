3972bca7f1b3ee5ba1e88ec1d1135a42692b735e
stella <sftamesis13@gmail.com>
2023-04-24 13:39:01 -0700

    set up methods for index, create, and show to test in postman

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            render json: user
+        else
+            render json: {errors: user.errors.full_messages}
+        end
+    end
+
+    def show 
+        user = find_user
+        if user
+            render json: user
+        else
+            render json: {error: "User not found"}
+        end
+    end
+
+    private
+
+    def find_user
+        user = User.find_by(id: params[:id])
+    end
+
+    def user_params
+        params.permit(:name, :username, :password, :password_confirmation)
+    end
 end
