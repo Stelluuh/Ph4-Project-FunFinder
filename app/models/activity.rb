df43a7e0420e6a7527b68ecf7fe50d9e66e5662b
stella <sftamesis13@gmail.com>
2023-04-24 12:02:35 -0700

    setup associations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,4 @@
 class Activity < ApplicationRecord
+    has_many :schedules
+    has_many :users, through: :schedules
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :schedules
+    has_many :activities, through: :schedules
+    has_secure_password
+    validates :username, uniqueness: true
 end
