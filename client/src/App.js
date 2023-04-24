06035ed7697a24f49beba5226325b24d3f7f844f
stella <sftamesis13@gmail.com>
2023-04-24 15:25:42 -0700

    setup basic code needed for file

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { UserProvider } from './context/AuthContext';
 import Home from './Home';
-import logo from './logo.svg';
+import Signup from './Signup';
+import Login from './Login';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <UserProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </UserProvider>
     </div>
   );
 }
--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -6,14 +6,25 @@ const UserContext = React.createContext();
 //This component will be used to wrap the entire application and provides the context to all the components
 const UserProvider = ({ children }) => {
   const [user, setUser] = useState({})
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
 
     useEffect(() => {
-        
+        fetch('/me')
+            .then(response => response.json())
+            .then(data => console.log(data))
     }, [])
 
+    const login =(user) => {
+        setUser(user)
+    }
+
+
+
   return (
-    <UserContext.Provider value={{ user, setUser }}>
+    <UserContext.Provider value={{ user }}>
       {children}
     </UserContext.Provider>
   )
 }
+
+export { UserContext, UserProvider }
