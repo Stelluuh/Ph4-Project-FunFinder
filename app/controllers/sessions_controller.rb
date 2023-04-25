708092c317833dddbe141c10c0ffa073397afb49
stella <sftamesis13@gmail.com>
2023-04-25 10:32:47 -0700

    setup login feature

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+    
+    #LOGIN
+    # from routes: post '/login', to: 'sessions#create'
+    def create
+        #first find the user by their username
+        user = User.find_by(username: params[:username])
+        #if the user exists and the password is correct then we login
+            # what this does is check if the user exists. adding .authenticate is a method given by bcrypt that checks if the password is correct.
+        if user&.authenticate(params[:password]) 
+            session[:user_id] = user.id # this is what actually logs the user in. It sets the session user_id to the user's id. This is how we keep track of the user that is logged in.
+        else # we render an error:
+            render json: { error: "Invalid username or password" }, status: :unauthorized
+        end
+    end
+    
+    #LOGOUT
+    # from routes: delete '/logout', to: 'sessions#destroy'
+    def destroy
+
+    end
+
+
+end
