deda99238b6b8278d04b03716c2ad71142004030
stella <sftamesis13@gmail.com>
2023-04-25 10:32:17 -0700

    Displays hello<username> on homepage after login

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { UserContext } from './context/AuthContext';
+
+// This is the home page that will be displayed when the user is logged in
+
 
 const Home = () => {
+
+  const navigate = useNavigate()
+  const { user, isLoggedIn } = useContext(UserContext)
+
+    if(isLoggedIn) {
+      return (
+        <div>
+          <h1>Hello {user.username}</h1>
+        </div>
+      )
+    } else {
+      <h3>Please Login or Signup</h3>
+      navigate('/login')
+    }
+  
   return (
     <div>
       <h1>Hello from Home Page</h1>
