859bd6ce361e8191a0419988e5cf0e7637e00cff
stella <sftamesis13@gmail.com>
2023-04-26 11:59:17 -0700

    refactor CRUD to ensure only users are associated with their account only

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,23 +1,27 @@
 class SchedulesController < ApplicationController
+    # I need to set up full CRUD in a way to ensure that any scheduleds created by a user are associeted with their account only.
+
+
+    # before_action: :set_schedule, only: [:show, :update, :destroy] # we call set_schedule to find the schedule that the user is trying to perform an action on
 
     def index
-        schedules = Schedule.all
+        schedules = current_user.schedules
         render json: schedules
     end
 
     def show
-        schedule = find_schedule
+        schedule = current_user.schedules.find_by(id: params[:id])
         render json: schedule
     end
 
     def create
-        schedule = Schedule.create!(schedule_params)
+        schedule = current_user.schedules.create!(schedule_params)
         render json: schedule, status: :created
     end
 
     def update
-        #find the schedule that matches the ID from route params
-        schedule = find_schedule
+        #find the schedule OF LOGGED IN USER that matches the ID from route params
+        schedule = current_user.schedules.find_by(id: params[:id])
         #update the schedule with the data from the form
         schedule.update!(schedule_params)
         render json: schedule, status: :accepted
@@ -25,7 +29,7 @@ class SchedulesController < ApplicationController
 
     def destroy
         #find the schedule that matches the ID from route params
-        schedule = find_schedule
+        schedule = current_user.schedules.find_by(id: params[:id])
         # if schedule exists, destroy it
         schedule.destroy
         head :no_content
@@ -33,12 +37,14 @@ class SchedulesController < ApplicationController
     end
 
     private
-    
-    def find_schedule
-        schedule = Schedule.find(params[:id])
+
+    def current_user
+        User.find_by(id: session[:user_id])
     end
 
     def schedule_params
         params.permit(:time_of_day, :user_id, :activity_id)
     end
+
+
 end
