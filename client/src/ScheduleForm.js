8a50af942a328874b719812874b13baf081a0fb5
stella <sftamesis13@gmail.com>
2023-04-26 14:45:44 -0700

    create ScheduleForm file and Schedeules.js files

---
+++ client/src/ScheduleForm.js
@@ -0,0 +1,33 @@
+import React, { useState, useContext } from 'react'
+import { UserContext } from './context/AuthContext';
+
+const ScheduleForm = ({ addSchedule }) => {
+    const [timeOfDay, setTimeOfDay] = useState('')
+
+    const {addSchedule} = useContext(UserContext)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        addSchedule({ // we are passing the schedule object to the addSchedule function in AuthContext.js
+            timeOfDay: timeOfDay // this comes from the form
+        })
+
+
+    }
+
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>Schedule Form</label>
+                <input 
+                    type="text"
+                    name="time of day"
+                    placeholder="time of day"
+                    value={timeOfDay}
+                    onChange = {(e) => setTimeOfDay(e.target.value)}
+                />
+                <input type="submit" value="Add Schedule" />
+            </form>
+        </div>
+    )
+}
---
+++ client/src/Schedules.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Schedules = () => {
+  return (
+    <div>
+      <h3>My Schedule</h3>
+    </div>
+  )
+}
+
+export default Schedules
