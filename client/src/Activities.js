ddda0b7a952168d369c90baf3644813b36752e55
stella <sftamesis13@gmail.com>
2023-05-01 09:55:05 -0700

    set state for all activities regardless of user, but user has to be logged in

--- client/src/Activities.js
+++ client/src/Activities.js
@@ -1,23 +1,58 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { UserContext } from './context/AuthContext'
+import ActivityCard from './ActivityCard'
 
 
 const Activities = () => {
-  const { user, isLoggedIn } = useContext(UserContext)
+  const { user, isLoggedIn, checkLogin } = useContext(UserContext)
+  const [allActivities, setAllActivities] = useState([])
+
+  // useEffect(() => {
+  //   if (isLoggedIn) {
+  //     fetch('/activities')
+  //     .then(res => res.json())
+  //     .then(data => console.log(data))
+  //   }
+  // }, [user])
 
   useEffect(() => {
-    if (isLoggedIn) {
-      fetch('/activities')
-      .then(res => res.json())
-      .then(data => console.log(data))
-    }
-  }, [user])
+    checkLogin()
+  }, [])
+
+  //All activities of all users.
+  useEffect(() => {
+    fetch('/activities')
+    .then(response => response.json())
+    .then(data => setAllActivities(data))
+  }, [isLoggedIn])
+
+
+  const renderAllActivities = allActivities.map(activity => {
+    return <ActivityCard key={activity.id} activity={activity}/>
+  })
+
+
+  // //Activities that belong to user only
+  // const userActivities = user?.activities?.map(
+  //   activity => {
+  //     return <ActivityCard key={activity.id} activity={activity}/>
+  //   }
+  // )
 
 
 
   return (
     <div>
-      <h1>Activities List</h1>
+      {isLoggedIn ? (
+        <>
+           <h1>Activities List</h1>
+           {/* {userActivities} */}
+           {renderAllActivities}
+
+        </>
+      ) : (
+        <h3>Please Login or Signup</h3>
+      )}
     </div>
   )
 }
