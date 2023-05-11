d6b8c15774b5c52fbc3e00ba0ef0885c5b5e9874
stella <sftamesis13@gmail.com>
2023-05-11 10:06:52 -0700

    remove comments

--- client/src/Schedules.js
+++ client/src/Schedules.js
@@ -33,31 +33,3 @@ const Schedules = () => {
 }
 
 export default Schedules
-
-
-// const Schedules = () => {
-//   const { user, isLoggedIn } = useContext(UserContext)
-  
-//   if (isLoggedIn) { 
-//     const scheduleList = user?.schedules?.map(schedule => { //adding the ? after user and schedules checks to see if they exits and it will prevent the error 'cannot read properties of undefined(reading 'map) from happening if the user is not logged in
-//       return <ScheduleCard key={schedule.id} schedule={schedule} />
-//     })
-
-//     return (
-//       <div>
-//         <h3>My Schedule</h3> 
-//         {scheduleList}     
-//         <ScheduleForm />
-//       </div>
-//     )
-//   } else {
-    
-//     return (
-//       <div>
-//         <h3>Please Login or Signup</h3>
-//       </div>
-//     )
-//   }
-  
-
-// }
