a9914b8bd85abf6df10114995fb00d95238ed905
stella <sftamesis13@gmail.com>
2023-05-12 10:09:40 -0700

    edit activities to lower case letters

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 
 puts 'Seeding database...'
 # activities
-activity1 = Activity.create(name: "Swimming Lessons", description: "Learn to swim with our certified instructors", childs_age: 5, duration: 30)
-activity2 = Activity.create(name: "Art Class", description: "Explore your creativity through painting, drawing and more", childs_age: 7, duration: 60)
-activity3 = Activity.create(name: "Soccer Practice", description: "Improve your soccer skills and have fun with friends", childs_age: 8, duration: 45)
-activity4 = Activity.create(name: "Piano Lessons", description: "Learn to play the piano with our experienced instructors", childs_age: 6, duration: 45)
-activity5 = Activity.create(name: "Cooking Class", description: "Discover the joys of cooking and baking", childs_age: 10, duration: 60)
-activity6 = Activity.create(name: "Basketball Practice", description: "Train with our coaches and improve your basketball skills", childs_age: 12, duration: 90)
-activity7 = Activity.create(name: "Dance Class", description: "Explore different dance styles and express yourself through movement", childs_age: 9, duration: 60)
-activity8 = Activity.create(name: "Yoga for Kids", description: "Experience the benefits of yoga and mindfulness", childs_age: 7, duration: 30)
+activity1 = Activity.create(name: "swimming lessons", description: "Learn to swim with our certified instructors", childs_age: 5, duration: 30)
+activity2 = Activity.create(name: "art class", description: "Explore your creativity through painting, drawing and more", childs_age: 7, duration: 60)
+activity3 = Activity.create(name: "soccer practice", description: "Improve your soccer skills and have fun with friends", childs_age: 8, duration: 45)
+activity4 = Activity.create(name: "piano lessons", description: "Learn to play the piano with our experienced instructors", childs_age: 6, duration: 45)
+activity5 = Activity.create(name: "cooking class", description: "Discover the joys of cooking and baking", childs_age: 10, duration: 60)
+activity6 = Activity.create(name: "basketball practice", description: "Train with our coaches and improve your basketball skills", childs_age: 12, duration: 90)
+activity7 = Activity.create(name: "dance class", description: "Explore different dance styles and express yourself through movement", childs_age: 9, duration: 60)
+activity8 = Activity.create(name: "yoga for kids", description: "Experience the benefits of yoga and mindfulness", childs_age: 7, duration: 30)
 
 
 # users
