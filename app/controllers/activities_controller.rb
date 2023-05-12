0a5ce65dd6b976d9366fe7493e5324ff516fad2b
stella <sftamesis13@gmail.com>
2023-05-12 12:09:46 -0700

    remove show, update and destroy functionality

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -9,28 +9,9 @@ class ActivitiesController < ApplicationController
         render json: activity, status: :created
     end
 
-       # def show
-    #     activity = find_activity
-    #     render json: activity
-    # end
-
-    # def update
-    #     activity = find_activity
-    #     activity.update!(activity_params)
-    #     render json: activity, status: :accepted
-    # end
-
-    # def destroy
-    #     activity = find_activity
-    #     activity.destroy
-    #     head :no_content
-    # end
 
     private
 
-    # def find_activity
-    #     activity = Activity.find(params[:id])
-    # end
 
     def activity_params
         params.permit(:name, :description, :childs_age, :duration)
