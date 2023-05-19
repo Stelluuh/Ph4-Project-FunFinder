08927c864a6aaabaf653d5d38b9ff09c805cd843
stella <sftamesis13@gmail.com>
2023-05-19 13:37:08 -0700

    remove practice custom serializer

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -1,10 +1,10 @@
 class ActivitySerializer < ActiveModel::Serializer
-  attributes  :name, :description, :childs_age, :duration, :summary, :id
+  attributes  :name, :description, :childs_age, :duration, :id
 
     has_many :schedules
     has_many :users, through: :schedules
 
-    def summary
-        "#{object.name} is an activity that lasts #{object.duration} minutes. #{object.description[0..25]}..."
-    end
+    # def summary
+    #     "#{object.name} is an activity that lasts #{object.duration} minutes. #{object.description[0..25]}..."
+    # end
 end
