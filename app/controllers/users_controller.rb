dfd3d858d7b7229e9293cd4890f4a8881bc4ab17
stella <sftamesis13@gmail.com>
2023-05-22 12:50:57 -0700

    delete comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,12 @@
 class UsersController < ApplicationController
-    skip_before_action :authorize, only: [:create] # we skip the authorize method for the create action because we don't want to authorize a user when they are signing up
+    skip_before_action :authorize, only: [:create] 
 
     # when a user signs up, they are automatically logged in
     def create
         user = User.create!(user_params) 
-        session[:user_id] = user.id # this is the line that logs the user in by saving their id in the session hash
+        session[:user_id] = user.id 
         render json: user, status: :created
     end
-
-    # when a user logs in, they are automatically logged in
-    # def show
-    #     user = find_by(id: session[:user_id])
-    #     render json: user, status: :ok
-    # end
 
     def show 
         render json: @current_user
