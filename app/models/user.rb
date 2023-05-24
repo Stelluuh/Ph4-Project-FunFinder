c35c252c26af70448bafc36930a4c6e81821e4c7
stella <sftamesis13@gmail.com>
2023-05-24 11:17:28 -0700

    delete comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_many :schedules
     has_many :activities, through: :schedules
 
-    has_secure_password # this line gives us the authenticate method
+    has_secure_password 
 
     validates :name, :username, :password, :password_confirmation, presence: true
     validates :username, uniqueness: true
