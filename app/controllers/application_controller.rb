cbddaa72ab19e831ce510a6d79e9aed72b11d7f7
stella <sftamesis13@gmail.com>
2023-06-06 10:57:36 -0700

    complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   before_action :authorize 
+
   private
 
   def render_not_found_response invalid
